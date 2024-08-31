9b9c32e29df1a4cdab7a2d519511232f8effe917
Ashwin145 <ashwinnair847>
2024-09-01 03:07:12 +0530

    Adding files

---
+++ app/build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.mynotemvvm"
+    compileSdk = 34
+
+    buildFeatures{
+        viewBinding  = true
+    }
+
+    defaultConfig {
+        applicationId = "com.example.mynotemvvm"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    val lifecycle_version = "2.8.4"
+    val arch_version = "2.2.0"
+    val room_version = "2.6.1"
+
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Annotation processor
+    implementation ("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
+
+    implementation ("androidx.room:room-runtime:2.5.2")
+    annotationProcessor( "androidx.room:room-compiler:2.5.2" ) // For Java
+}
---
+++ app/src/main/java/com/example/mynotemvvm/NoteDao.java
@@ -0,0 +1,26 @@
+package com.example.mynotemvvm;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao  // Add this annotation
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("SELECT * FROM my_notes")  // Ensure table name matches the @Entity annotation
+    LiveData<List<Note>> getAllData();
+}
